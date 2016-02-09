62b8b5618d113b3d653ab36d4bae0bbbaaeec5a8
fgerthoffert <f.gerthoffert@gmail.com>
2016-02-09 08:39:05 -0500

    Fixed error in build script

--- deploy-gdc-docs.sh
+++ deploy-gdc-docs.sh
@@ -25,7 +25,7 @@ sed -i -e 's/# / /g' docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.p
 sed -i -e 's/### /## /g' docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pd
 sed -i -e 's/\/site\//\/docs\//g' docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pd
 echo "$(date +'%d %B %Y - %k:%M'): Data_Submission_Portal UG: Building PDF from pandoc document "
-pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions -o docs/Data_Portal/PDF/Data_Submission_Portal_UG.pdf docs/Data_Portal/PDF/Data_Submission_Portal_UG.pd
+pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions -o docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pdf docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pd
 
 if [ ! -d "docs/API/PDF/" ]; then
    mkdir docs/API_UG/PDF/
@@ -37,7 +37,7 @@ sed -i -e 's/# / /g' docs/API/PDF/API_UG.pd
 sed -i -e 's/### /## /g' docs/API/PDF/API_UG.pd
 sed -i -e 's/\/site\//\/docs\//g' docs/API/PDF/API_UG.pd
 echo "$(date +'%d %B %Y - %k:%M'): API UG: Building PDF from pandoc document "
-pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions -o docs/Data_Portal/PDF/API_UG.pdf docs/Data_Portal/PDF/API_UG.pd
+pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions -o docs/API/PDF/API_UG.pdf docs/API/PDF/API_UG.pd
 
 if [ ! -d "docs/Data_Transfer_Tool/PDF/" ]; then
    mkdir docs/Data_Transfer_Tool_UG/PDF/
@@ -49,4 +49,4 @@ sed -i -e 's/# / /g' docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pd
 sed -i -e 's/### /## /g' docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pd
 sed -i -e 's/\/site\//\/docs\//g' docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pd
 echo "$(date +'%d %B %Y - %k:%M'): Data_Transfer_Tool UG: Building PDF from pandoc document "
-pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions -o docs/Data_Portal/PDF/Data_Transfer_Tool_UG.pdf docs/Data_Portal/PDF/Data_Transfer_Tool_UG.pd
+pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions -o docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pdf docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pd
