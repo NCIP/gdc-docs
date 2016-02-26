9404312db2b1d172162e186ad47c7afba5dc5bb3
Michael Moncada <michael.moncada@oicr.on.ca>
2016-02-26 16:24:55 -0500

    * Some cleanup of code.

--- theme/js/dictionary-init.js
+++ theme/js/dictionary-init.js
@@ -3,6 +3,62 @@ window.onload = function() {
 
   window.gdcApp = window.gdcApp || {};
 
+  // For all multi-value options identified by the dictionary
+  // create a simple accordion component that will allow us to
+  // hide/show more values
+  function _initMultiValueAccordions(numberOfShowingItems) {
+
+    var accordions = jQuery('.bullets');
+
+    if (accordions.length === 0) {
+      return;
+    }
+
+    accordions.parent().append('<div class="accordion-more-less-toggle" />');
+
+    var bullets = accordions.find('li li');
+
+    if (bullets.length === 0) {
+      return;
+    }
+
+    var liHeight = bullets.outerHeight(),
+      valueHeight = numberOfShowingItems * liHeight;
+
+    accordions.each(function () {
+      var accordion = jQuery(this);
+
+      if (accordion.outerHeight() > valueHeight) {
+        accordion.css({overflow: 'hidden', height: valueHeight});
+        accordion.parent().find('.accordion-more-less-toggle')
+          .css({marginLeft: '2.2rem'})
+          .append('<a href="javascript:void(0)" class="more-less-vals" title="Click to see more values."><i class="fa fa-angle-down"></i> More Values</a>');
+      }
+    });
+
+    jQuery('.more-less-vals').click(function () {
+      var moreLessToggle = jQuery(this),
+        bullets = moreLessToggle.parent().parent().find('.bullets'),
+        title = 'Click to see less values.',
+        html = '<i class="fa fa-angle-up"></i> Less Values</a>',
+        css = {overflow: 'visible', height: ''};
+
+      if (moreLessToggle.hasClass('more')) {
+        css = {overflow: 'hidden', height: valueHeight};
+        html = '<i class="fa fa-angle-down"></i> More Values</a>';
+      }
+
+      bullets.css(css);
+      moreLessToggle.attr('title', title).html(html);
+
+      moreLessToggle.toggleClass('more');
+    });
+
+  }
+
+  //////////////////////////////////////////////////////
+  // Init function for our dictionary
+  //////////////////////////////////////////////////////
   function _init() {
     var dictionaryContainer = document.getElementById('dictionary-app-container');
 
@@ -10,8 +66,8 @@ window.onload = function() {
       return;
     }
 
-    var dictionaryPreamble = $('#dictionary-preamble'),
-        body = $('body'),
+    var dictionaryPreamble = jQuery('#dictionary-preamble'),
+        body = jQuery('body'),
         previousView = null;
 
     var dictionaryOptions = {
@@ -38,50 +94,7 @@ window.onload = function() {
 
         previousView = currentView;
 
-        var accordions = jQuery('.bullets');
-
-        if (accordions.length) {
-          accordions.parent().append('<div class="accordion-more-less-toggle" />');
-
-          var bullets = accordions.find('li li');
-
-          if (bullets.length === 0) {
-            return;
-          }
-
-          var liHeight = bullets.outerHeight(),
-              valueHeight = 6 * liHeight;
-
-          accordions.each(function() {
-            var $this = $(this);
-
-            if($this.outerHeight() > valueHeight) {
-              $this.css({overflow: 'hidden', height: valueHeight});
-              $this.parent().find('.accordion-more-less-toggle')
-                .css({marginLeft: '2.2rem'})
-                .append('<a href="javascript:void(0)" class="more-less-vals" title="Click to see more values."><i class="fa fa-angle-down"></i> More Values</a>');
-            }
-          })
-
-          jQuery('.more-less-vals').click(function(){
-            var $this = $(this);
-
-
-
-            if($this.hasClass('more')){
-              $this.parent().parent().find('.bullets').css({overflow: 'hidden', height: valueHeight});
-              $this.attr('title', 'Click to see more values.').html('<i class="fa fa-angle-down"></i> More Values</a>');
-            }
-            else {
-              $this.parent().parent().find('.bullets').css({overflow: 'visible', height: ''});
-              $this.attr('title', 'Click to see less values.').html('<i class="fa fa-angle-up"></i> Less Values</a>');
-            }
-
-            $this.toggleClass('more');
-          });
-        }
-
-
+        _initMultiValueAccordions(6); // 5 values + 1 header
 
       }
     };
