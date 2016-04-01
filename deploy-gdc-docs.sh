9e98bcafa0fa02310ceb366d22472f8cbfef7001
fgerthoffert <f.gerthoffert@gmail.com>
2016-04-01 09:44:15 -0400

    Updated build script to make PDF images clickable (only in staging/prod)

--- deploy-gdc-docs.sh
+++ deploy-gdc-docs.sh
@@ -65,60 +65,10 @@ for userGuide in "${userGuides[@]}"; do
    /bin/sed -i -e 's/# / /g' docs/${userGuide}/PDF/${userGuide}_UG.pd
    /bin/sed -i -e 's/### /## /g' docs/${userGuide}/PDF/${userGuide}_UG.pd
    /bin/sed -i -e 's/\/site\//\/docs\//g' docs/${userGuide}/PDF/${userGuide}_UG.pd
+   /bin/sed -i -e "s/(images/(https:\/\/gdc-docs.nci.nih.gov\/"$userGuide"\/Users_Guide\/images/g" docs/${userGuide}/PDF/${userGuide}_UG.pd #To make images clickable in the PDF
    echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: ${userGuide}: Building PDF from pandoc document "
    /usr/bin/pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions docs/${userGuide}/PDF/${userGuide}_Title.txt -o docs/${userGuide}/PDF/${userGuide}_UG.pdf docs/${userGuide}/PDF/${userGuide}_UG.pd
 done
-
-: <<'COMMENT'
-if [ ! -d "docs/Data_Portal/PDF/" ]; then
-   mkdir docs/Data_Portal/PDF/
-fi
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Data_Portal UG: Building pandoc document"
-/usr/local/bin/mkdocs2pandoc -f Data_Portal_UG.yml -o docs/Data_Portal/PDF/Data_portal_UG.pd
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Data_Portal UG: Replacing strings in pandoc document "
-/bin/sed -i -e 's/# / /g' docs/Data_Portal/PDF/Data_portal_UG.pd
-/bin/sed -i -e 's/### /## /g' docs/Data_Portal/PDF/Data_portal_UG.pd
-/bin/sed -i -e 's/\/site\//\/docs\//g' docs/Data_Portal/PDF/Data_portal_UG.pd
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Data_Portal UG: Building PDF from pandoc document "
-/usr/bin/pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions docs/Data_Portal/PDF/Data_Portal_Title.txt -o docs/Data_Portal/PDF/Data_portal_UG.pdf docs/Data_Portal/PDF/Data_portal_UG.pd
-
-if [ ! -d "docs/Data_Portal/PDF/" ]; then
-   mkdir docs/Data_Submission_Portal_UG/PDF/
-fi
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Data_Submission_Portal UG: Building pandoc document"
-/usr/local/bin/mkdocs2pandoc -f Data_Submission_Portal_UG.yml -o docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pd
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Data_Submission_Portal UG: Replacing strings in pandoc document "
-/bin/sed -i -e 's/# / /g' docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pd
-/bin/sed -i -e 's/### /## /g' docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pd
-/bin/sed -i -e 's/\/site\//\/docs\//g' docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pd
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Data_Submission_Portal UG: Building PDF from pandoc document "
-/usr/bin/pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions docs/Data_Submission_Portal/PDF/Data_Submission_Portal_Title.txt -o docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pdf docs/Data_Submission_Portal/PDF/Data_Submission_Portal_UG.pd
-
-if [ ! -d "docs/API/PDF/" ]; then
-   mkdir docs/API/PDF/
-fi
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: API UG: Building pandoc document"
-/usr/local/bin/mkdocs2pandoc -f API_UG.yml -o docs/API/PDF/API_UG.pd
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: API UG: Replacing strings in pandoc document "
-/bin/sed -i -e 's/# / /g' docs/API/PDF/API_UG.pd
-/bin/sed -i -e 's/### /## /g' docs/API/PDF/API_UG.pd
-/bin/sed -i -e 's/\/site\//\/docs\//g' docs/API/PDF/API_UG.pd
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: API UG: Building PDF from pandoc document "
-/usr/bin/pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions docs/API/PDF/API_Title.txt -o docs/API/PDF/API_UG.pdf docs/API/PDF/API_UG.pd
-
-if [ ! -d "docs/Data_Transfer_Tool/PDF/" ]; then
-   mkdir docs/Data_Transfer_Tool/PDF/
-fi
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Data_Transfer_Tool UG: Building pandoc document"
-/usr/local/bin/mkdocs2pandoc -f Data_Transfer_Tool_UG.yml -o docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pd
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Data_Transfer_Tool UG: Replacing strings in pandoc document "
-/bin/sed -i -e 's/# / /g' docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pd
-/bin/sed -i -e 's/### /## /g' docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pd
-/bin/sed -i -e 's/\/site\//\/docs\//g' docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pd
-echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Data_Transfer_Tool UG: Building PDF from pandoc document "
-/usr/bin/pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_Title.txt -o docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pdf docs/Data_Transfer_Tool/PDF/Data_Transfer_Tool_UG.pd
-
-COMMENT
 
 echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Cleaning previous website directory (rm)"
 sudo rm /var/www/gdc-docs-${ENVIRONMENT}.nci.nih.gov/* -R
