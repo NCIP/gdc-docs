553e45f68c14fadb05b17c487a3e73fa2a8c3861
fgerthoffert <f.gerthoffert@gmail.com>
2016-06-15 18:21:50 -0400

    Updated script to build new code block in PDF

--- deploy-gdc-docs.sh
+++ deploy-gdc-docs.sh
@@ -64,7 +64,7 @@ if $hasEncodingError  ; then
    if [ -f /tmp/${ENVIRONMENT}-buildlog.txt ]; then
       echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Copying log file"
       cp /tmp/${ENVIRONMENT}-buildlog.txt /var/www/gdc-docs-${ENVIRONMENT}.nci.nih.gov/buildlog.txt
-   fi   
+   fi
    exit
 fi
 
@@ -103,7 +103,7 @@ for userGuide in "${userGuides[@]}"; do
    /bin/sed -i -e 's/\/site\//\/docs\//g' docs/${userGuide}/PDF/${userGuide}_UG.pd
    /bin/sed -i -e "s/(images/(https:\/\/gdc-docs.nci.nih.gov\/"$userGuide"\/Users_Guide\/images/g" docs/${userGuide}/PDF/${userGuide}_UG.pd #To make images clickable in the PDF
    echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: ${userGuide}: Building PDF from pandoc document "
-   /usr/bin/pandoc --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions docs/${userGuide}/PDF/${userGuide}_Title.txt -o docs/${userGuide}/PDF/${userGuide}_UG.pdf docs/${userGuide}/PDF/${userGuide}_UG.pd
+   /usr/bin/pandoc --listings -H theme/latex/listings-setup.tex --toc -V documentclass=report -V geometry:"top=2cm, bottom=1.5cm, left=1cm, right=1cm" -f markdown+grid_tables+table_captions docs/${userGuide}/PDF/${userGuide}_Title.txt -o docs/${userGuide}/PDF/${userGuide}_UG.pdf docs/${userGuide}/PDF/${userGuide}_UG.pd
 done
 
 echo "$(date +'%d %B %Y - %k:%M'): ${ENVIRONMENT}: Cleaning previous website directory (rm)"
