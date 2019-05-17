0302c86e6da63b13ba055ebb65148c26abaf8940
Sam Rich <samrichca@users.noreply.github.com>
2019-05-17 14:46:46 -0400

    refactoring

--- theme/js/homepage.js
+++ theme/js/homepage.js
@@ -1,66 +1,33 @@
 $(function () {
-  var $searchContainer = $('.hp-search');
-  var $inputBox = $('.hp-search__input');
-  var searchInput = document.getElementById('hp-search__input');
-  var $resultsWrapper = $('.hp-search__wrapper-results');
-  var $resultsContainer = $('.hp-search__results-container');
-  var $results = $('.hp-search__results');
-  var $searchContentBody = $('.hp-search__body');
-  var $cancelButton = $('.hp-search__cancel');
-  var _VALID_QUERY_LENGTH = 3;
   var _isSearchActive = false;
   var _searchItemClass = 'hp-search__item';
+  var _VALID_QUERY_LENGTH = 3;
+  var $cancelButton = $('.hp-search__cancel');
+  var $inputBox = $('.hp-search__input');
+  var $results = $('.hp-search__results');
+  var $resultsWrapper = $('.hp-search__wrapper-results');
+  var $searchContainer = $('.hp-search');
+  var $searchContentBody = $('.hp-search__body');
 
   $inputBox.focus();
 
-  $inputBox.on('blur', function () {
-    var $this = $(this);
-    if ($this.val() === '') {
-      $this.closest('.hp-search').removeClass('search-active');
-    }
-  });
-
   function _debounce(func, wait, immediate) {
     var _timeout;
-
     return function () {
-      var context = this,
-        args = arguments,
-        later = function () {
-          _timeout = null;
-          if (!immediate) func.apply(context, args);
-        };
-
+      var context = this;
+      var args = arguments;
+      var later = function () {
+        _timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
       var callNow = immediate && !_timeout;
-
       clearTimeout(_timeout);
-
       _timeout = setTimeout(later, wait);
-
       if (callNow) {
         func.apply(context, args);
       }
     };
   }
-
-  function _resetSearch() {
-    $inputBox.val('');
-    $resultsWrapper.hide();
-
-  }
-
-  $resultsContainer.on('click keyup', '.' + _searchItemClass, function (e) {
-    if (e.type !== 'click' && e.which !== 13 && e.which !== 32) return;
-
-    e.stopPropagation();
-    e.preventDefault();
-
-    var href = $(this).data('link') || null;
-
-    if (href) window.location.href = href;
-
-    _resetSearch();
-  });
 
   $.get(base_url + '/mkdocs/search_index.json', function (data) {
     var index = lunr(function () {
@@ -130,11 +97,11 @@ $(function () {
           doc.base_url = base_url;
           doc.summary = doc.text.substring(0, 200);
 
-          resultsHTML += '<div class="' + _searchItemClass + ' animated fadeInTop" tabindex="0" role="button" data-link="' + doc.location + '">' +
+          resultsHTML += '<a class="' + _searchItemClass + ' animated fadeInTop" href="' + doc.location + '">' +
             '<div class="search-body">' +
             '<h3>' + doc.title + '</h3>' +
             '<p>' + doc.summary + '</p>' +
-            '</div></div>';
+            '</div></a>';
         }
 
         $results.append(resultsHTML);
@@ -144,7 +111,7 @@ $(function () {
       }
     }
 
-    searchInput.addEventListener('keyup', _debounce(_search, 300));
+    $inputBox.on('keyup', _debounce(_search, 300));
 
     $cancelButton.on('click', function () {
       $inputBox.val('');
