d7d23d0f074e6a22e78009ee33aae466ee65b7f9
Sam Rich <samrichca@users.noreply.github.com>
2020-03-11 14:22:25 -0400

    get CDE from dictionary terms

--- theme/apps/dictionary/scripts/dictionary-views.js
+++ theme/apps/dictionary/scripts/dictionary-views.js
@@ -314,7 +314,7 @@
             property = dictionaryProperties[propertyName],
             description = _.get(property, 'term.description', false) || _.get(property, 'description', false) || _.get(dictionaryTerms, propertyName + '.description', 'n/a'),
             valueOrType = _getPropertyValueOrType(property),
-            CDE = _.get(dictionaryTerms, propertyName + '.termDef'),
+            CDE = _.get(dictionaryTerms, propertyName + '.common.termDef.cde_id'),
             isRequired = requiredProperties && requiredProperties.indexOf(propertyName) >= 0 ? 'Yes' : 'No';
 
         // Ignore system properties for now...
